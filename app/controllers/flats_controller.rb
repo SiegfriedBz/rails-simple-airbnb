f31abb73d2158e27f562ffa0519264255cf7f9af
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-26 20:56:29 +0200

    added flat model and flats controller

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,44 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+end
---
+++ app/models/flat.rb
@@ -0,0 +1,8 @@
+class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_night, presence: true
+  validates :number_of_guests, presence: true
+
+end
